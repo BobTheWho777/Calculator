a271079963b283ec7fbdc54bf9152957c4364799
Elvida2004 <147031392+Elvida2004@users.noreply.github.com>
2024-10-17 16:29:03 +0300

    Revert "Метод сложения"
    This reverts commit 4250771a2c600d1bdd003eae41a2f32c5fd78594.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //Сложение
                 Scanner scanner = new Scanner(System.in);
                 int num1 = scanner.nextInt();
                 int num2 = scanner.nextInt();
-                int sum = num1 - num2;
+                int sum = num1 + num2;
     }
 }
